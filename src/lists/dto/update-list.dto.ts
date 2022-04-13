ecdb76fac6c15e83cb36378d546c8f809ecf6897
Maxim Mihaylin <slimbox.sdb@gmail.com>
2022-04-14 04:42:07 +0600

    dev(): GetUser decorator

--- src/lists/dto/update-list.dto.ts
+++ src/lists/dto/update-list.dto.ts
@@ -1,12 +1,16 @@
-import { IsString, MaxLength, MinLength } from "class-validator";
+// import { IsString, MaxLength, MinLength } from "class-validator";
+//
+// export class UpdateListDto {
+//   @MinLength(2, {
+//     message: 'Name of the list is too short'
+//   })
+//   @MaxLength(50, {
+//     message: 'Name of the list is too long'
+//   })
+//   @IsString()
+//   name: string;
+// }
 
-export class UpdateListDto {
-  @MinLength(5, {
-    message: 'Name of the list is too short'
-  })
-  @MaxLength(50, {
-    message: 'Name of the list is too long'
-  })
-  @IsString()
-  name: string;
-}
+import { CreateListDto } from "./create-list.dto";
+
+export type UpdateListDto = Partial<CreateListDto>;
